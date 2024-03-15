d538e832f5ba71042638be3a73561983e1aa6148
lukasz <l.bielecki13@gmail.com>
2024-03-15 20:00:42 +0100

    adding new requests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 4000;
 const app = express();
 app.use(express.json());
 
-const databse = {
+const database = {
   users: [
     {
       id: '123',
@@ -30,10 +30,52 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === databse.users[0].email && req.body.password === databse.users[0].password) {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json('success');
   } else {
     res.status(400).json('error logging in');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
   }
 });
 
