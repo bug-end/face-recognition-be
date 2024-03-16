ed2dc00abdcd80096c809dc4f2d30515c53826d7
lukasz <l.bielecki13@gmail.com>
2024-03-16 20:46:28 +0100

    fixing cors issues

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import bcrypt from 'bcryptjs';
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
