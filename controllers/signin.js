e6c50ab588d75ead83a9f9080baadf90515ebc8f
lukasz <l.bielecki13@gmail.com>
2024-03-20 22:36:36 +0100

    adding basic validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,20 @@
 export const handleSignin = async (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+
   try {
-    const data = await db.select('email', 'hash').from('login').where('email', '=', req.body.email);
+    const data = await db.select('email', 'hash').from('login').where('email', '=', email);
 
     if (data.length === 0) {
       return res.status(400).json('wrong credentials');
     }
 
-    const isValid = await bcrypt.compare(req.body.password, data[0].hash);
+    const isValid = await bcrypt.compare(password, data[0].hash);
 
     if (isValid) {
-      const user = await db.select('*').from('users').where('email', '=', req.body.email);
+      const user = await db.select('*').from('users').where('email', '=', email);
       if (user.length === 0) {
         return res.status(400).json('unable to get user');
       }
