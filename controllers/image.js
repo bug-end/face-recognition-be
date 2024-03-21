24f00159c0f9faed034be88a7b9a333b4a7324ae
lukasz <l.bielecki13@gmail.com>
2024-03-21 19:43:40 +0100

    adding clarifai api call

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,47 @@
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: process.env.CLARIFAI_USER_ID,
+      app_id: process.env.CLARIFAI_APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + process.env.CLARIFAI_PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
+export const handleApiCall = (req, res) => {
+  const { input } = req.body;
+  fetch(
+    'https://api.clarifai.com/v2/models/' + process.env.CLARIFAI_MODEL_ID + '/outputs',
+    returnClarifaiRequestOptions(input)
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 export const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import knex from 'knex';
 import { handleRegister } from './controllers/register.js';
 import { handleSignin } from './controllers/signin.js';
 import { handleGetProfile } from './controllers/profile.js';
-import { handleImage } from './controllers/image.js';
+import { handleImage, handleApiCall } from './controllers/image.js';
 
 dotenv.config();
 
@@ -31,6 +31,7 @@ app.post('/signin', (req, res) => handleSignin(req, res, db, bcrypt));
 app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => handleGetProfile(req, res, db));
 app.put('/image', (req, res) => handleImage(req, res, db));
+app.post('/imageurl', (req, res) => handleApiCall(req, res));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
