6f9380ac2b961840b134f77bfc4be9bf18b33daf
lukasz <l.bielecki13@gmail.com>
2024-03-26 22:46:53 +0100

    replacing REST API call with gRPC

--- controllers/image.js
+++ controllers/image.js
@@ -1,45 +1,38 @@
-const returnClarifaiRequestOptions = (imageUrl) => {
-  const IMAGE_URL = imageUrl;
+import { ClarifaiStub, grpc } from 'clarifai-nodejs-grpc';
 
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: process.env.CLARIFAI_USER_ID,
-      app_id: process.env.CLARIFAI_APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'Key ' + process.env.CLARIFAI_PAT,
-    },
-    body: raw,
-  };
-
-  return requestOptions;
-};
+const stub = ClarifaiStub.grpc();
 
 export const handleApiCall = (req, res) => {
   const { input } = req.body;
-  fetch(
-    'https://api.clarifai.com/v2/models/' + process.env.CLARIFAI_MODEL_ID + '/outputs',
-    returnClarifaiRequestOptions(input)
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.status(400).json('unable to work with API'));
+
+  const metadata = new grpc.Metadata();
+  metadata.set('authorization', `Key ${process.env.CLARIFAI_PAT}`);
+
+  stub.PostModelOutputs(
+    {
+      model_id: 'face-detection',
+      user_app_id: {
+        user_id: process.env.CLARIFAI_USER_ID,
+        app_id: process.env.CLARIFAI_APP_ID,
+      },
+      inputs: [{ data: { image: { url: input } } }],
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        console.log('Error: ' + err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+
+      if (response.status.code !== 10000) {
+        console.log('Received failed status: ' + response.status.description + '\n' + response.status.details);
+        res.status(400).json({ error: 'Bad Request' });
+        return;
+      }
+      res.json(response);
+    }
+  );
 };
 
 export const handleImage = (req, res, db) => {
